7bbf1799e77a1c24fe28f5ef6185112f28ed4824
Owynn <angelaleman2212@gmail.com>
2025-06-15 15:03:13 -0600

    Welcome page updated

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,19 +11,17 @@ export default function Page() {
               Hey, I'm Owynn!
             </h1>
             <p className="p-2 text-lg text-gray-600 mb-6 text-shadow-sm max-w-2xl">
-              This is a simple webpage built with Next.js and Tailwind CSS. <br></br> Explore
-              the content and enjoy the design!
+              This is a simple webpage built with Next.js and Tailwind CSS.{" "}
+              <br></br> Explore the content and enjoy the design!
             </p>
           </div>
-          <div>
-            <Image
-              width={700}
-              height={700}
-              src="/webpage-background-idea.png"
-              alt="webpage-background-idea"
-              className="min-w-[300px] h-auto"
-            />
-          </div>
+          <Image
+            width={700}
+            height={700}
+            src="/webpage-background-idea.png"
+            alt="webpage-background-idea"
+            className="min-w-[300px] h-auto"
+          />
         </div>
         <div className="bg-white/30 backdrop-blur-md shadow-lg rounded-xl px-8 py-4 flex items-center font-sans justify-center max-w-7xl w-full mx-auto mt-6">
           <section className="p-8 my-12 mx-auto max-w-2xl">
@@ -31,10 +29,13 @@ export default function Page() {
               A little about me
             </h2>
             <p className="text-gray-700 text-lg leading-relaxed">
-               I'm a Systems Engineering student from Mexico who enjoys meeting new people,
-               discussing contemporary issues, and embracing new experiences. I'm fluent in both English and Spanish.
-               I'm passionate about fitness and spend a lot of time at the gym, but I'm just as comfortable focusing on long-term 
-               challenges that require persistence and patience. I'm always ready to face new obstacles and excited to connect with you!
+              I'm a Systems Engineering student from Mexico who enjoys meeting
+              new people, discussing contemporary issues, and embracing new
+              experiences. I'm fluent in both English and Spanish. I'm
+              passionate about fitness and spend a lot of time at the gym, but
+              I'm just as comfortable focusing on long-term challenges that
+              require persistence and patience. I'm always ready to face new
+              obstacles and excited to connect with you!
             </p>
           </section>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import Footer from "./components/footer";
+import Image from "next/image";
 
 const page = () => {
   return (
     <div className="min-h-screen flex flex-col bg-[#f2f5d5]">
       <main className="flex-1 flex flex-col items-center justify-center w-full px-4">
+        <Image
+          width={300}
+          height={300}
+          src="/owynn.png"
+          alt="Owynn's Portfolio"
+          className="min-w-[300px] h-auto mb-8"
+          />
         <h1 className="text-center p-2 text-5xl font-extrabold bg-gray-700 bg-clip-text text-transparent mb-8 drop-shadow-lg">
           Welcome to Owynn's Portfolio
         </h1>
         <div className="w-full flex justify-center">
-          <a href="/home">
-            <button className="px-8 py-3 rounded-full text-gray-700 font-bold shadow-lg border-2 border-gray-900 hover:bg-gradient-to-r from-green-500 via-orange-400 to-red-600 hover:text-black hover:border-gray-600 transition-all duration-200 ease-in-out tracking-wide text-lg">
-              Go to Home
-            </button>
-          </a>
         </div>
       </main>
       <div className="mb-8">
